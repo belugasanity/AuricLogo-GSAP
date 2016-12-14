a9bb5c03684ba9c683772a5f4ce08a82af7ea6b4
belugasanity <aust.bagley@gmail.com>
2016-12-14 13:11:00 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+TweenMax.from(".logo", 3, {opacity:0, rotation:360, x:600, ease:Elastic.easeOut.config(1, 0.3)});
+TweenMax.from("#ring1", 5, {opacity:0, rotation:360, ease:Back.easeOut.config(1.7)});
+TweenMax.from("#ring2", 4, {opacity:0, rotation:360, ease:Back.easeOut.config(1.7)});
+TweenMax.from("#ring3", 3, {opacity:0, rotation:360, ease:Back.easeOut.config(1.7)});
+TweenMax.from("#ring4", 2, {opacity:0, rotation:360, ease:Back.easeOut.config(1.7)});
+TweenMax.from("#ring5", 1, {opacity:0, rotation:360, ease:Back.easeOut.config(1.7)});
+TweenMax.from("#text", 3, {opacity:0, delay:2, x:20});
